c29b92c7e213a5d1c9420b8c5b1f3c054c32fbf6
Pruttibodee Adam <154861856+GetzInsomnia@users.noreply.github.com>
2025-06-10 14:24:25 +0700

    Add additional LocalBusiness fields

--- src/components/StructuredData.tsx
+++ src/components/StructuredData.tsx
@@ -5,6 +5,19 @@ const structuredData = {
   "@type": "LocalBusiness",
   url: "https://virintira.com/",
   name: "Virintira Accounting Office",
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "222/172 ถนนสามวา แขวงบางชัน",
+    addressLocality: "เขตคลองสามวา กรุงเทพมหานคร",
+    postalCode: "10510",
+    addressCountry: "TH",
+  },
+  telephone: "+66928825556",
+  sameAs: [
+    "https://www.facebook.com/AccountbyVirintira/",
+    "https://www.tiktok.com/@virintiraa",
+    "https://lin.ee/AbavzHa",
+  ],
 }
 
 export default function StructuredData() {
