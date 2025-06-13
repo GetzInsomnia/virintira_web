7a0df095743023555227f19878bde29ab38662e8
Pruttibodee Adam <154861856+GetzInsomnia@users.noreply.github.com>
2025-06-14 02:20:48 +0700

    Add host and protocol redirection

--- server.js
+++ server.js
@@ -8,6 +8,27 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
+    if (!dev) {
+      const protoHeader = req.headers['x-forwarded-proto'];
+      const proto = Array.isArray(protoHeader) ? protoHeader[0] : protoHeader;
+      const hostHeader = req.headers.host || '';
+      const host = hostHeader.split(':')[0];
+
+      if (proto && proto !== 'https') {
+        res.writeHead(301, { Location: `https://${hostHeader}${req.url}` });
+        res.end();
+        return;
+      }
+
+      if (host.toLowerCase() === 'www.virintira.com') {
+        res.writeHead(301, {
+          Location: `https://virintira.com${req.url}`,
+        });
+        res.end();
+        return;
+      }
+    }
+
     handle(req, res);
   }).listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`);
