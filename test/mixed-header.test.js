5f15ce3390f4ffd0e0561498293db748126192ce
GetzInsomnia <get-insomnia@live.com>
2025-06-19 11:59:04 +0700

    Delete test redirect / not use redirect due to deployment on Plesk

--- test/mixed-header.test.js
+++
@@ -1,6 +0,0 @@
-const assert = require('assert');
-const { shouldRedirectToHttps } = require('../server');
-
-const req = { headers: { 'x-forwarded-proto': 'https,http' } };
-assert.strictEqual(shouldRedirectToHttps(req), false, 'should not redirect when header includes https');
-console.log('mixed-header test passed');
